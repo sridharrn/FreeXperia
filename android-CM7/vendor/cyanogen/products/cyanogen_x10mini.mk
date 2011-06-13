e2629700aae761b5329a07572b0685961c343c3a
unknown <rojera01@.(none)>
2011-06-13 21:16:13 +0300

    semc devices and 32 kernel with tinyrcu

--- android-CM7/vendor/cyanogen/products/cyanogen_x10mini.mk
+++ android-CM7/vendor/cyanogen/products/cyanogen_x10mini.mk
@@ -1,8 +1,8 @@
-# Inherit AOSP device configuration for passion.
-$(call inherit-product, device/se/x10mini/full_x10mini.mk)
+# Inherit AOSP device configuration for x10Mini.
+$(call inherit-product, device/se/x10mini/device_x10mini.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cyanogen/products/common.mk)
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
 # Include GSM stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
@@ -11,29 +11,14 @@ $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 # Setup device specific product configuration.
 #
 PRODUCT_NAME := cyanogen_x10mini
-PRODUCT_BRAND := SE
+PRODUCT_BRAND := SonyEricsson
 PRODUCT_DEVICE := x10mini
-PRODUCT_MODEL := X10mini
-PRODUCT_MANUFACTURER := SE
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=x10mini BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ22 BUILD_FINGERPRINT=google/passion/passion:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.4 GRJ22 121341 release-keys"
+PRODUCT_MODEL := X10 Mini
+PRODUCT_MANUFACTURER := SonyEricsson
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SonyEricsson_x10Mini BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
 
-PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-x10mini.map
 
-# Add LDPI assets, in addition to MDPI
-PRODUCT_LOCALES += ldpi mdpi
-
-# Extra overlay for LDPI
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
-
-# Enable Windows Media
-WITH_WINDOWS_MEDIA := true
-
-# Add the Torch app
-PRODUCT_PACKAGES += Torch
-
-# Add the Cyanogenmod Wallpapers
-PRODUCT_PACKAGES += CMWallpapers
-
+PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/device/se/x10Mini/prelink-linux-arm-x10mini.map
 
 #
 # Set ro.modversion
@@ -44,16 +29,15 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.4-X10Mini
+            ro.modversion=CyanogenMod-7.0.2-X10Mini
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.4-X10Mini-MiniCM7-2.0.2
+            ro.modversion=CyanogenMod-7.0.2-X10Mini-000
     endif
 endif
 
 #
-# Copy bootanimation
+# Copy specific prebuilt files
 #
 PRODUCT_COPY_FILES +=  \
-    vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
-
+    vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
