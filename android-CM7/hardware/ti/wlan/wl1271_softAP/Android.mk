229a569559128dd2327a4832b9b7890dc35b2d40
jerpelea <jerpelea@gmail.com>
2011-07-22 08:20:28 +0300

    semc:removed merged patches and devices

--- android-CM7/hardware/ti/wlan/wl1271_softAP/Android.mk
+++
@@ -1,3 +0,0 @@
-ifeq ($(BOARD_SOFTAP_DEVICE),wl1271a)
-    include $(call all-subdir-makefiles)
-endif
--- android-CM7/hardware/ti/wlan/wl1271_softAP/platforms/os/linux/zoom2_env.bash
+++
@@ -1,5 +0,0 @@
-export PATH=<PATH TO CROSS COMPILER>:$PATH
-export CROSS_COMPILE=arm-none-linux-gnueabi-
-export ARCH=arm
-export HOST_PLATFORM=zoom2
-export KERNEL_DIR=<PATH TO KERNEL DIR>
